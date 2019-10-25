cf8e8601b1010152cbf05196d812e2cc6b372157
459829mu <55836864+459829mu@users.noreply.github.com>
2019-10-25 19:01:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -138,7 +138,7 @@ $(function() {
   	$('#text').show();
 
   	$("#description").keyup(function(){
-  	  $("#count").text("Characters left: " + (400 - $(this).val().length));
+  	  $("#count").text("Počet znaků: " + ($(this).val().length));
   	});
 
   	$('#submit_text').on('click',function() {
